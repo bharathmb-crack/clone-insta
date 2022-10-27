69cab60ffde38f7bdc855ad4c96d8bc388439d24
Bharath byahatti <67644782+bharathmb-crack@users.noreply.github.com>
2022-10-27 11:44:45 +0530

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,8 +3,8 @@ import GoogleProvider from "next-auth/providers/google";
 export const authOptions = {
   providers: [
     GoogleProvider({
-      clientId:process.env.NEXT_PUBLIC_GOOGLE_ID,
-      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_SECRET,
+      clientId:`${process.env.NEXT_PUBLIC_GOOGLE_ID}`,
+      clientSecret: `${process.env.NEXT_PUBLIC_GOOGLE_ID}`,
     }),
   ],
   pages: {
