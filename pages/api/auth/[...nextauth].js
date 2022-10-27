2ef90edfd9f77e79d8227eb3091cd1bd34fea50d
Bharath byahatti <67644782+bharathmb-crack@users.noreply.github.com>
2022-10-27 12:30:34 +0530

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,7 +4,7 @@ export const authOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.NEXT_PUBLIC_GOOGLE_ID,
-      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_ID,
+      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_SECRET,
     }),
   ],
   pages: {
