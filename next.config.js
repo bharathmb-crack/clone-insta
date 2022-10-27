3e3d783662ab7ef984e2d5b7cc4b88bb17c13a62
Bharath byahatti <67644782+bharathmb-crack@users.noreply.github.com>
2022-10-27 12:00:31 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,5 +7,10 @@ module.exports = {
       "images.unsplash.com",
     ],
   },
+  env: {
+    NEXT_PUBLIC_GOOGLE_ID:
+      "498508007741-0umr818i6rg9i806s863jjo2nfliciqe.apps.googleusercontent.com",
+    NEXT_PUBLIC_GOOGLE_SECRET: "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",
+  },
   reactStrictMode: true,
 };
